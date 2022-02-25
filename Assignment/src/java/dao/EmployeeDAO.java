387c64d7ce55f158a087c9c70c02e502ba6988b4
anhtnhe150123 <anhtnhe150123@fpt.edu.vn>
2022-02-25 20:24:49 +0700

    srcc

---
+++ Assignment/src/java/dao/EmployeeDAO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import context.DBContext;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Employee;
+
+/**
+ *
+ * @author Apple
+ */
+public class EmployeeDAO {
+
+    public List<Employee> getAllEmployees() {
+        List<Employee> list = new ArrayList<>();
+        try {
+            String sql = "select * from EMPLOYEE";
+            Connection conn = new DBContext().getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Employee employee = Employee.builder()
+                        .id(rs.getInt(1))
+                        .name(rs.getString(2))
+                        .dateBirth(rs.getString(3))
+                        .gender(rs.getString(4))
+                        .position(rs.getString(5))
+                        .startDate(rs.getString(6)).build();
+                list.add(employee);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(EmployeeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+    
+}
---
+++ Assignment/src/java/model/Employee.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author Apple
+ */
+@Builder
+@Getter
+@Setter
+@ToString
+public class Employee {
+    private int id;
+    private String name;
+    private String dateBirth;
+    private String gender;
+    private String position;
+    private String startDate;
+}
